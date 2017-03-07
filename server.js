611b3b85c1def843b1b903d11fd35a44ec98d86f
夜の町 <yecheng2005s@qq.com>
2017-03-08 00:38:38 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 var http = require('http');
 var port = Number(process.env.PORT || 3000);
 
-var server = http.createServer((req, res) => {
-    if (req.url !== '/favicon.ico') {
-        res.write("port:" + port);
-        res.end("aa");
-    }
-});
-server.listen(port,function(){
-    var str = this;
+var opt = {
+ host:'http://yecheng-node.herokuapp.com',
+ port:port,
+ method:'POST',
+ path:' https://www.google.com',
+ headers:{
+  //这里放期望发送出去的请求头
+ }
+}
+//以下是接受数据的代码
+var body = '';
+var req = http.request(opt, function(res) {
+  console.log("Got response: " + res.statusCode);
+  res.on('data',function(d){
+  body += d;
+ }).on('end', function(){
+  console.log(res.headers)
+  console.log(body)
+ });
+
+}).on('error', function(e) {
+  console.log("Got error: " + e.message);
 })
+req.end();
